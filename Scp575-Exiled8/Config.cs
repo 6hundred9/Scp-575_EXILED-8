51ca7d80ece9dee6cb442551d2a8e5326f69bf00
6hundred9 <96420090+RobloxianCoder@users.noreply.github.com>
2024-03-26 21:22:31 +0300

    Add files via upload

---
+++ Scp575-Exiled8/Config.cs
@@ -0,0 +1,21 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scp575_Exiled8
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        public int spawnChance = 0;
+        public int minSpawnTime = 0;
+        public int maxSpawnTime = 0;
+        public int minBlackoutTime = 0;
+        public int maxBlackoutTime = 0;
+        public int timesPerRound = 0;
+    }
+}
