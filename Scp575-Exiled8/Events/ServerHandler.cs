fea3da3f482f60cbe95f9accb41881048aef9d8f
6hundred9 <96420090+6hundred9@users.noreply.github.com>
2024-03-27 15:08:49 +0300

    Add files via upload

---
+++ Scp575-Exiled8/Events/ServerHandler.cs
@@ -0,0 +1,54 @@
+﻿
+using Exiled.API.Features;
+using PluginAPI.Events;
+using System;
+using System.Collections;
+using System.Threading;
+using MEC;
+using System.Collections.Generic;
+using Exiled.Events.EventArgs.Server;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Exiled.Events.EventArgs.Player;
+
+namespace Scp575_Exiled8.Events
+{
+    public class ServerHandler
+    {
+        private static Config config = Scp575.Instance.Config;
+        private static int funny = Scp575.editable;
+        private static MEC.CoroutineHandle yes;
+        public static void OnRoundStarted()
+        {
+            yes = Timing.RunCoroutine(iHaveNoIdeaWhatToNameThis());
+        }
+
+        public static void OnRoundEnded(RoundEndedEventArgs ev)
+        {
+            Timing.KillCoroutines(yes);
+            funny = config.timesPerRound;
+        }
+        
+
+        public static IEnumerator<float> iHaveNoIdeaWhatToNameThis()
+        {
+            for (; ; )
+            {
+                if (funny <= 0) break; Cassie.Message(config.recontainCassie);
+
+
+                yield return Random.Range(config.minSpawnTime, config.maxSpawnTime);
+
+                int smth = Random.Range(config.minBlackoutTime, config.maxSpawnTime);
+                if (Random.Range(config.spawnChance, 101) == 100)
+                {
+                    if (funny == config.timesPerRound) Cassie.Message(config.breachCassie);
+                    Map.TurnOffAllLights(smth);
+                    funny--;
+                    yield return Random.Range(config.minSpawnTime, config.maxSpawnTime);
+                    
+                }
+            }
+        }
+    }
+}
