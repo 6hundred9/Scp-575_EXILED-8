c60516c5f97cef0af16a4f2078aae62ae56bb0e9
6hundred9 <96420090+RobloxianCoder@users.noreply.github.com>
2024-03-27 12:42:43 +0300

    Add files via upload

---
+++ Scp575-Exiled8/Events/ServerHandler.cs
@@ -0,0 +1,47 @@
+﻿
+using Exiled.API.Features;
+using PluginAPI.Events;
+using System;
+using System.Collections;
+using System.Threading;
+using MEC;
+using System.Collections.Generic;
+using Exiled.Events.EventArgs.Server;
+
+namespace Scp575_Exiled8.Events
+{
+    public class ServerHandler
+    {
+        private static Config config = Scp575.Instance.Config;
+        private static int funny = Scp575.editable;
+        public static void OnRoundStarted()
+        {
+            Timing.RunCoroutine(iHaveNoIdeaWhatToNameThis());
+        }
+
+        public static void OnRoundEnded(RoundEndedEventArgs ev)
+        {
+            Timing.KillCoroutines();
+            funny = config.timesPerRound;
+        }
+
+        public static IEnumerator<float> iHaveNoIdeaWhatToNameThis()
+        {
+            for (; ; )
+            {
+                if (funny <= 0) { 
+                    
+                    break; }
+                Random rnd = new Random();
+
+                yield return rnd.Next(config.minSpawnTime, config.maxSpawnTime);
+
+                if (rnd.Next(config.spawnChance, 101) == 100)
+                {
+                    Map.TurnOffAllLights(rnd.Next(config.minBlackoutTime, config.maxBlackoutTime));
+                    funny--;
+                }
+            }
+        }
+    }
+}
